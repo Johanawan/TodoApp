779cd6441e45385eb3980fbf9c4fc0dd5a6470f0
johanawan <johanawan@hotmail.com>
2021-07-17 17:35:55 +0100

    Add Django Rest Framework

--- notes/settings.py
+++ notes/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'api.apps.ApiConfig',
+
+    # 3rd Party Apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
